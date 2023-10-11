d52e049dbb1c242c203dc13d89d98c7f457b602b
chanho lee <leech3878@naver.com>
2023-10-11 19:37:37 +0900

    Initial Commit

---
+++ BeforeIn/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  BeforeIn
+//
+//  Created by t2023-m079 on 10/11/23.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
---
+++ BeforeIn/MainPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeforeIn
+//
+//  Created by t2023-m079 on 10/11/23.
+//
+
+import UIKit
+
+class MainViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("로드완료")
+    }
+
+
+}
+
