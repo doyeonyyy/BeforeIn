705a054019178af3056c62ac9e6ef5e0c6cdc376
Soobean <saen5848@naver.com>
2023-10-12 14:48:33 +0900

    [Feat] : ProfileViewController UI 작성 중, 텍스트 라벨 생성

--- BeforeIn/ProfilePage/ProfileViewController.swift
+++ BeforeIn/ProfilePage/ProfileViewController.swift
@@ -12,18 +12,36 @@ class ProfileViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Create a UILabel for nickname
+        let nickname = UILabel()
+        nickname.text = "000님"
+        nickname.textColor = UIColor(red: 0.192, green: 0.192, blue: 0.192, alpha: 1)
+        nickname.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        nickname.translatesAutoresizingMaskIntoConstraints = false
+        nickname.textAlignment = .center // Center text alignment
+
+        self.view.addSubview(nickname)
+
+        NSLayoutConstraint.activate([
+            nickname.heightAnchor.constraint(equalToConstant: 29),
+            nickname.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 60),
+            nickname.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 24)
+        ])
+
+        // Create a UILabel for '의 프로필 입니다'
+        let mentLabel = UILabel()
+        mentLabel.text = "의 프로필 입니다"
+        mentLabel.textColor = UIColor(red: 0.192, green: 0.192, blue: 0.192, alpha: 1)
+        mentLabel.font = UIFont.systemFont(ofSize: 18)
+        mentLabel.translatesAutoresizingMaskIntoConstraints = false
+        mentLabel.textAlignment = .center // Center text alignment
+
+        self.view.addSubview(mentLabel)
+
+        NSLayoutConstraint.activate([
+            mentLabel.heightAnchor.constraint(equalToConstant: 29),
+            mentLabel.topAnchor.constraint(equalTo: nickname.topAnchor),
+            mentLabel.leadingAnchor.constraint(equalTo: nickname.trailingAnchor, constant: 2)
+        ])
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
