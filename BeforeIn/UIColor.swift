733e99d87b0ad9c16680e2c85a278e00458fa6af
sanc93 <sanc93@me.com>
2023-10-12 12:21:35 +0900

    add: BeforeInRed, BeforeInBlue 에셋 추가

---
+++ BeforeIn/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  BeforeIn
+//
+//  Created by Sanghun K. on 10/12/23.
+//
+
+import UIKit
+
+extension UIColor {
+    /// BeforeInRed - red: 0.616, green: 0.102, blue: 0.102, alpha: 1
+    class var BeforeInRed: UIColor? { return UIColor(named: "BeforeInRed") }
+    /// BeforeInBlue - red: 0.140, green: 0.200, blue: 0.670, alpha: 1
+    class var BeforeInBlue: UIColor? { return UIColor(named: "BeforeInBlue") }
+}
