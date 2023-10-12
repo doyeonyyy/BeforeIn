84c6f3015f50b12098dbeccb8c33870af67d2ded
sanc93 <sanc93@me.com>
2023-10-12 16:07:30 +0900

    chore: 검색창, 취소버튼 생성

--- BeforeIn/SearchPage/SearchViewController.swift
+++ BeforeIn/SearchPage/SearchViewController.swift
@@ -5,25 +5,59 @@
 //  Created by t2023-m079 on 10/11/23.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 class SearchViewController: BaseViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    private lazy var searchTextField = UITextField().then {
+        $0.placeholder = "검색어를 입력하세요"
+        $0.borderStyle = .none
+        $0.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1)
+        $0.layer.cornerRadius = 10
+        $0.rightView = searchButton
+        $0.rightViewMode = .unlessEditing
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let searchButton = UIButton().then{
+        $0.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        $0.tintColor = .gray // TODO: 색상 정하기(figma에 지정된 색 없음)
     }
-    */
+    
+    private let cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .none
+    }
+  
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Methods & Selectors
+    func configureUI() {
+        navigationController?.isNavigationBarHidden = true // 네비게이션 바를 숨김
 
+        // addSubview
+        view.addSubview(searchTextField)
+        view.addSubview(cancelButton)
+        
+        // makeConstraints
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20) // TODO: top 60을 어디를 기준으로 잡아야할지..
+            $0.leading.equalToSuperview().offset(24)
+            $0.trailing.equalTo(cancelButton.snp.leading).offset(-16)
+            $0.height.equalTo(50)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.centerY.equalTo(searchTextField)
+            $0.trailing.equalToSuperview().offset(-24)
+        }
+        
+        
+    }
 }
