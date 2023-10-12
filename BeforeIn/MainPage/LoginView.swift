71fd83053cefb9ca1e028f29769eacb647e38f08
sanc93 <sanc93@me.com>
2023-10-12 20:29:33 +0900

    Squashed commit of the following:
    commit b1824f70acd6531cd119ebf96fe4ba3f6e01d95e
    Merge: 5fb927f abea7d8
    Author: Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
    Date:   Thu Oct 12 20:23:03 2023 +0900
    
        Merge pull request #2 from lcho3878/ui/login-page
    
        feat: 로그인페이지 UI 구현
    
    commit abea7d841e40ab6c9efa18f174055465b3fae491
    Author: Leehan-sol <ho20128@naver.com>
    Date:   Thu Oct 12 20:20:02 2023 +0900
    
        chore: RootView 테스트용 코드 수정 (로그인페이지 -> 메인페이지)
    
    commit af66d4f3eb92a1059f5824c76ce99356650e7491
    Author: Leehan-sol <ho20128@naver.com>
    Date:   Thu Oct 12 20:15:04 2023 +0900
    
        chore: 로그인페이지 버튼 간격 조정
    
    commit 94f91b60ff9f0bcd9aae0e4630f920414fd1a053
    Author: Leehan-sol <ho20128@naver.com>
    Date:   Thu Oct 12 19:27:36 2023 +0900
    
        feat: 로그인페이지 버튼 func 생성
    
    commit 3ddd4994bf4dd9deba1d1a4b4fc3894cff4cad3b
    Author: Leehan-sol <ho20128@naver.com>
    Date:   Thu Oct 12 17:55:28 2023 +0900
    
        feat: 로그인페이지 UI 완성
    
    commit 70b107b7985e5368231a1ba58e4be9dc2e377efb
    Author: Leehan-sol <ho20128@naver.com>
    Date:   Thu Oct 12 16:14:26 2023 +0900
    
        feat: 아이디, 비밀번호 텍스트필드 생성

---
+++ BeforeIn/MainPage/LoginView.swift
@@ -0,0 +1,199 @@
+//
+//  LoginView.swift
+//  BeforeIn
+//
+//  Created by t2023-m0048 on 2023/10/12.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class LoginView: UIView {
+    
+    // MARK: - UI Properties
+    lazy var idView = UIView().then {
+        $0.layer.borderColor = UIColor.systemGray2.cgColor
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 8
+        $0.layer.masksToBounds = true
+        $0.addSubview(idTextField)
+        $0.addSubview(idLabel)
+    }
+    
+    lazy var idTextField = UITextField().then {
+        $0.autocapitalizationType = .none
+        $0.autocorrectionType = .no
+        $0.spellCheckingType = .no
+        $0.keyboardType = .emailAddress
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 0))
+        $0.leftViewMode = .always
+    }
+    
+    lazy var idLabel = UILabel().then {
+        $0.text = "아이디를 입력하세요."
+        $0.font = UIFont.systemFont(ofSize: 18)
+        $0.textColor = .systemGray2
+    }
+    
+    lazy var pwView = UIView().then {
+        $0.layer.borderColor = UIColor.systemGray2.cgColor
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 8
+        $0.layer.masksToBounds = true
+        $0.addSubview(pwTextField)
+        $0.addSubview(pwLabel)
+    }
+    
+    lazy var pwTextField = UITextField().then {
+        $0.autocapitalizationType = .none
+        $0.autocorrectionType = .no
+        $0.spellCheckingType = .no
+        $0.isSecureTextEntry = true
+        $0.clearsOnBeginEditing = false
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 0))
+        $0.leftViewMode = .always
+    }
+    
+    lazy var pwLabel = UILabel().then {
+        $0.text = "비밀번호를 입력하세요."
+        $0.font = UIFont.systemFont(ofSize: 18)
+        $0.textColor = .systemGray2
+    }
+    
+    lazy var maintainLabel = UILabel().then {
+        $0.text = "로그인 상태 유지"
+        $0.font = UIFont.systemFont(ofSize: 16)
+    }
+    
+    lazy var maintainButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "square"), for: .normal)
+        $0.tintColor = .black
+    }
+    
+    lazy var maintainStackView = UIStackView().then {
+        $0.addArrangedSubview(maintainLabel)
+        $0.addArrangedSubview(maintainButton)
+        $0.spacing = 5
+        $0.axis = .horizontal
+    }
+    
+    lazy var loginButton = UIButton().then {
+        $0.setTitle("로그인", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.backgroundColor = .BeforeInRed
+        $0.layer.cornerRadius = 8
+    }
+    
+    lazy var findIdButton = UIButton().then {
+        $0.setTitle("아이디 찾기", for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    lazy var findPwButton = UIButton().then {
+        $0.setTitle("비밀번호 찾기", for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    lazy var registerButton = UIButton().then {
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    lazy var buttonStackView = UIStackView().then {
+        $0.addArrangedSubview(findIdButton)
+        $0.addArrangedSubview(findPwButton)
+        $0.addArrangedSubview(registerButton)
+        $0.spacing = 16
+        $0.axis = .horizontal
+        $0.alignment = .fill
+    }
+    
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        addSubview()
+        setUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Methods
+    func addSubview(){
+        addSubview(idView)
+        addSubview(pwView)
+        addSubview(maintainStackView)
+        addSubview(loginButton)
+        addSubview(buttonStackView)
+    }
+    
+   // lazy var idLabelCenterYConstraint = idLabel.snp.centerY.equalTo(idView)
+   // lazy var pwLableCenterYConstraint = pwLabel.centerY.equalTo(pwView)
+    
+    func setUI(){
+        idLabel.snp.makeConstraints {
+            $0.left.equalTo(idView.snp.left).offset(8)
+            $0.right.equalTo(idView.snp.right).offset(-8)
+            $0.centerY.equalTo(idView)
+        }
+        
+        idTextField.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.edges.equalTo(idView)
+        }
+        
+        idView.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top).offset(321)
+            $0.left.right.equalTo(self.safeAreaLayoutGuide).inset(24)
+        }
+        
+        pwLabel.snp.makeConstraints {
+            $0.left.equalTo(pwView.snp.left).offset(8)
+            $0.right.equalTo(pwView.snp.right).offset(-8)
+            $0.centerY.equalTo(pwView)
+        }
+        
+        pwTextField.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.edges.equalTo(pwView)
+        }
+        
+        pwView.snp.makeConstraints {
+            $0.top.equalTo(idView.snp.bottom).offset(8)
+            $0.left.right.equalTo(self.safeAreaLayoutGuide).inset(24)
+        }
+        
+        maintainStackView.snp.makeConstraints{
+            $0.top.equalTo(pwView.snp.bottom).offset(17)
+            $0.left.equalTo(self.safeAreaLayoutGuide.snp.left).offset(25)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(maintainStackView.snp.bottom).offset(40)
+            $0.left.right.equalTo(self.safeAreaLayoutGuide).inset(24)
+            $0.height.equalTo(50)
+        }
+        
+        buttonStackView.snp.makeConstraints{
+            $0.top.equalTo(loginButton.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    
+    
+    
+}
+
+
+
