cb323d6ca4d9664fe530dfefa95c1f026196256e
sanc93 <sanc93@me.com>
2023-10-14 13:12:32 +0900

    chore: 디테일페이지 - 디테일뷰1 기본 UI 구현

--- BeforeIn/DetailPage/FirstDetailView.swift
+++ BeforeIn/DetailPage/FirstDetailView.swift
@@ -13,7 +13,24 @@ class FirstDetailView: UIView {
     
     // MARK: - UI Properties
     lazy var detailImageView = UIView().then {
-        $0.backgroundColor = .yellow
+        $0.backgroundColor = .darkGray
+    }
+    
+    lazy var backButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        $0.tintColor = .white
+    }
+    
+    lazy var titleLabel = UILabel().then {
+        $0.text = "결혼식장 에티켓 알아보기"
+        $0.font = .boldSystemFont(ofSize: 32)
+        $0.textColor = .white
+    }
+    
+    lazy var descriptionLabel = UILabel().then {
+        $0.text = "결혼식장 방문에서 지켜야할 기본 규칙을 알아봅시다"
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .white
     }
     
     // MARK: - Life Cycle
@@ -32,14 +49,32 @@ class FirstDetailView: UIView {
     
     func addSubview() {
         addSubview(detailImageView)
+        addSubview(backButton)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
         
     }
     
     func setUI(){
-        detailImageView.snp.makeConstraints{
+        detailImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(detailImageView.snp.top).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+            $0.width.height.equalTo(34)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.bottom.equalTo(descriptionLabel.snp.top).offset(-8)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-88)
+            $0.leading.equalToSuperview().offset(16)
+        }
     }
     
 }
