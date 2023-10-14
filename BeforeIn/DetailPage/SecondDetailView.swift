b8bd5eb8c626c6de1ef5650bb394e08c0d0a633c
sanc93 <sanc93@me.com>
2023-10-14 14:09:41 +0900

    chore: 디테일페이지 - 디테일뷰2 레이블 추가

--- BeforeIn/DetailPage/SecondDetailView.swift
+++ BeforeIn/DetailPage/SecondDetailView.swift
@@ -13,7 +13,31 @@ class SecondDetailView: UIView {
     
     // MARK: - UI Properties
     lazy var detailImageView = UIView().then {
-        $0.backgroundColor = .systemPink
+        $0.backgroundColor = .white
+    }
+    
+    lazy var titleEmoji = UILabel().then {
+        $0.text = "🚨"
+        $0.font = .systemFont(ofSize: 32)
+        $0.textColor = .black
+        $0.numberOfLines = 0
+    }
+    
+    lazy var titleLabel = UILabel().then {
+        $0.text = "이것만은 절대 금지!!"
+        $0.font = .systemFont(ofSize: 22, weight: .black)
+        $0.textColor = .black
+        $0.numberOfLines = 0
+    }
+    
+    lazy var descriptionLabel = UILabel().then {
+        $0.text = """
+        세상에서 한번 뿐인 행복한 날
+        모두를 위해서 피해야하는 언행을 모았습니다.
+        """
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .black
+        $0.numberOfLines = 0
     }
     
     // MARK: - Life Cycle
@@ -31,14 +55,29 @@ class SecondDetailView: UIView {
     
     
     func addSubview() {
-       addSubview(detailImageView)
-
+        addSubview(detailImageView)
+        addSubview(titleEmoji)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
     }
     
     func setUI(){
         detailImageView.snp.makeConstraints{
             $0.edges.equalToSuperview()
         }
+        titleEmoji.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(86)
+            $0.leading.equalToSuperview().offset(24)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleEmoji.snp.bottom).offset(5)
+            $0.leading.equalToSuperview().offset(24)
+        }
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(24)
+            $0.leading.equalToSuperview().offset(24)
+        }
+        
     }
     
 }
