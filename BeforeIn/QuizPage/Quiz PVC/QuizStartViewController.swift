15494016e5e4ff4a600917cf740f27ad1ae8a0c5
Daisy Kim <doyeonyyy@gmail.com>
2023-10-16 15:19:17 +0900

    feat: QuizStartViewController 업데이트

--- BeforeIn/QuizPage/Quiz PVC/QuizStartViewController.swift
+++ BeforeIn/QuizPage/Quiz PVC/QuizStartViewController.swift
@@ -11,5 +11,15 @@ class QuizStartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    func checkQuizRun() {
+        let userDefault = UserDefaults.standard
+        if userDefault.bool(forKey: "Quiz") == false {
+            let quizVC = QuizViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+            quizVC.modalPresentationStyle = .fullScreen
+            present(quizVC, animated: false)
+        }
     }
 }
