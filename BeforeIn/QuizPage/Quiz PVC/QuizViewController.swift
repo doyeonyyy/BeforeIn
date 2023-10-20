b9bf76dfafbd34921d3cefbce8206136cb493135
Daisy Kim <doyeonyyy@gmail.com>
2023-10-20 20:58:51 +0900

    add: ImageSet으로 비활성화된 OX버튼 Assets 추가 및 관련 코드 추가

--- BeforeIn/QuizPage/Quiz PVC/QuizViewController.swift
+++ BeforeIn/QuizPage/Quiz PVC/QuizViewController.swift
@@ -14,46 +14,42 @@ class QuizViewController: UIPageViewController {
     
     let ref = Database.database().reference()
     
-    private var previousButton: UIButton!
-    private var cancelButton: UIButton!
+    private var previousButton: UIButton?
+    private var cancelButton: UIButton?
     private var progressView = UIProgressView(progressViewStyle: .default)
     
-    private var buttonO: UIButton!
-    private var buttonX: UIButton!
-    private var nextButton: UIButton!
+    private var buttonO: UIButton?
+    private var buttonX: UIButton?
+    private var nextButton: UIButton?
     
-    private var pages = [UIViewController]() // 1. 'pages' 배열 선언 및 초기화
+    private var pages = [QuizContentsViewController]()
+    //    private var pages = [UIViewController]() // 1. 'pages' 배열 선언 및 초기화
     private var currentPageIndex = 0
+    var userAnswers = [UserAnswer]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
         setupLayout()
         setupPage()
         
-        previousButton.addTarget(self, action: #selector(goToPreviousPage), for: .touchUpInside)
-        cancelButton.addTarget(self, action: #selector(buttonHandler), for: .touchUpInside)
-        nextButton.addTarget(self, action: #selector(goToNextPage), for: .touchUpInside)
+        previousButton?.addTarget(self, action: #selector(goToPreviousPage), for: .touchUpInside)
+        cancelButton?.addTarget(self, action: #selector(buttonHandler), for: .touchUpInside)
+        nextButton?.addTarget(self, action: #selector(goToNextPage), for: .touchUpInside)
     }
     
     private func setupPage() {
         fetchRandomQuizQuestions { quizData in
             self.pages.removeAll()
-
+            
             for (index, quiz) in quizData.enumerated() {
-                let quizPage = QuizContentsViewController(page: "\(index + 1)/\(quizData.count)", content: quiz.question)
+                let quizPage = QuizContentsViewController(page: "\(index + 1)/\(quizData.count)", content: quiz.question, answer: quiz.answer)
                 self.pages.append(quizPage)
             }
             
             if let initialPage = self.pages.first {
                 self.setViewControllers([initialPage], direction: .forward, animated: true, completion: nil)
             }
-
-            // ProgressView 업데이트
-            let totalPageCount = self.pages.count - 1
-            self.progressView.setProgress(0.0, animated: false)
-            self.progressView.setProgress(1.0 / Float(totalPageCount), animated: true)
         }
     }
     
@@ -62,7 +58,7 @@ class QuizViewController: UIPageViewController {
         
         // Firebase 데이터베이스에서 퀴즈 데이터 가져오기
         ref.child("savedData/Quiz/place/결혼식/").observeSingleEvent(of: .value) { (snapshot) in
-            if let data = snapshot.value as? [String: Bool] {
+            if let data = snapshot.value as? [String: String] {
                 for (question, answer) in data {
                     let quizItem = Quiz(question: question, answer: answer)
                     quizArray.append(quizItem)
@@ -80,6 +76,8 @@ class QuizViewController: UIPageViewController {
         
         progressView = UIProgressView()
         progressView.progress = 0.0
+        // 처음에는 previousButton을 숨김
+        previousButton?.isHidden = true
         previousButton = UIButton()
         cancelButton = UIButton()
         buttonO = UIButton()
@@ -89,43 +87,40 @@ class QuizViewController: UIPageViewController {
         self.dataSource = self
         self.delegate = self
         
-        view.addSubview(previousButton)
-        view.addSubview(cancelButton)
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(previousButton!)
+        view.addSubview(cancelButton!)
         view.addSubview(progressView)
-        view.addSubview(buttonO)
-        view.addSubview(buttonX)
-        view.addSubview(nextButton)
+        view.addSubview(buttonO!)
+        view.addSubview(buttonX!)
+        view.addSubview(nextButton!)
+        
         
         progressView.tintColor = UIColor.BeforeInRed
-   
-        previousButton = UIButton()
-        previousButton.setTitle("이전 단계로", for: .normal)
-        previousButton.setTitleColor(.black, for: .normal)
         
-        cancelButton = UIButton()
-        cancelButton.setTitle("종료", for: .normal)
-        cancelButton.setTitleColor(.black, for: .normal)
+        previousButton?.setTitle("이전 단계로", for: .normal)
+        previousButton?.setTitleColor(.black, for: .normal)
         
-        buttonO = UIButton()
-        buttonO.setImage(UIImage(named: "O"), for: .normal)
+        cancelButton?.setTitle("종료", for: .normal)
+        cancelButton?.setTitleColor(.black, for: .normal)
         
-        buttonX = UIButton()
-        buttonX.setImage(UIImage(named: "X"), for: .normal)
+        buttonO?.setImage(UIImage(named: "O"), for: .normal)
+        buttonX?.setImage(UIImage(named: "X"), for: .normal)
         
-        nextButton = UIButton()
-        nextButton.setTitle("다음", for: .normal)
-        nextButton.setTitleColor(.black, for: .normal)
+        nextButton?.setTitle("다음", for: .normal)
+        nextButton?.setTitleColor(.black, for: .normal)
     }
     
     private func setupLayout() {
         
-        previousButton.snp.makeConstraints { make in
+        previousButton?.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(60)
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(24)
             make.width.height.equalTo(30)
         }
         
-        cancelButton.snp.makeConstraints { make in
+        cancelButton?.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(60)
             make.trailing.equalTo(view.safeAreaLayoutGuide).inset(24)
         }
@@ -134,24 +129,60 @@ class QuizViewController: UIPageViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
             make.centerX.equalTo(view)
         }
-
-        buttonO.snp.makeConstraints { make in
+        
+        buttonO?.snp.makeConstraints { make in
             make.width.height.equalTo(92)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(328)
             make.leading.equalTo(view.safeAreaLayoutGuide).offset(40)
         }
         
-        buttonX.snp.makeConstraints { make in
+        buttonX?.snp.makeConstraints { make in
             make.width.height.equalTo(162)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(296)
-            make.leading.equalTo(buttonO.snp.trailing).offset(100)
+            make.leading.equalTo((buttonO?.snp.trailing)!).offset(100)
             make.trailing.equalTo(view.safeAreaLayoutGuide).inset(30.89)
         }
         
-        nextButton.snp.makeConstraints { make in
+        nextButton?.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(626)
             make.centerX.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+    @objc func oButtonTapped(_ sender: UIButton) {
+        buttonX?.setImage(UIImage(named: "inactiveX"), for: .normal)
+        buttonO?.setImage(UIImage(named: "O"), for: .normal)
+        print("'O'를 선택하였습니다.")
+    }
+
+    @objc func xButtonTapped(_ sender: UIButton) {
+        buttonO?.setImage(UIImage(named: "inactiveO"), for: .normal)
+        buttonX?.setImage(UIImage(named: "X"), for: .normal)
+        print("'X'를 선택하였습니다.")
+    }
+
+    @objc func optionButtonTapped(_ sender: UIButton) {
+        let selectedOption = sender.tag == 0 ? "O" : "X"
+        
+        // 현재 질문에 대한 정답 확인
+        let currentQuestion = pages[currentPageIndex]
+        let isCorrect = (selectedOption == currentQuestion.answer)
+        
+        // 사용자 답변 저장
+        let userAnswer = UserAnswer(isCorrect: isCorrect, selectedOption: selectedOption)
+        userAnswers.append(userAnswer)
+        
+        // 다음 페이지로 이동
+        currentPageIndex += 1
+        if currentPageIndex < pages.count {
+            let nextQuestion = pages[currentPageIndex]
+            setViewControllers([nextQuestion], direction: .forward, animated: true, completion: nil)
+        } else {
+            // 마지막 페이지에 도달했을 때, 점수 및 결과 페이지 표시
+            showResultPage()
+        }
+    }
+    @objc func showResultPage() {
+//        self.present(QuizResultViewController, animated: true, completion: nil)
     }
 }
 
@@ -159,12 +190,12 @@ class QuizViewController: UIPageViewController {
 
 extension QuizViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewContorller: UIViewController) -> UIViewController? {
-        guard let currentIndex = pages.firstIndex(of: viewContorller) else { return nil }
+        guard let currentIndex = pages.firstIndex(of: viewContorller as! QuizContentsViewController) else { return nil }
         guard currentIndex > 0 else { return nil }
         return pages[currentIndex - 1]
     }
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        guard let currentIndex = pages.firstIndex(of: viewController as! QuizContentsViewController) else { return nil }
         guard currentIndex < (pages.count - 1) else { return nil }
         return pages[currentIndex + 1]
     }
@@ -173,8 +204,8 @@ extension QuizViewController: UIPageViewControllerDataSource {
 extension QuizViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         guard let viewControllers = pageViewController.viewControllers,
-              let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
-
+              let currentIndex = pages.firstIndex(of: viewControllers[0] as! QuizContentsViewController) else { return }
+        
         // 페이지의 진행률을 설정합니다.
         let totalPageCount = pages.count - 1
         let currentPage = Float(currentIndex)
@@ -199,7 +230,7 @@ extension QuizViewController {
         }
         animateButtonLayoutIfNeeded()
     }
-
+    
     @objc func pageControlHandler(_ sender: UIPageControl) {
         // 페이지 컨트롤이 변경될 때, 선택한 페이지로 이동합니다.
         let selectedPageIndex = sender.currentPage
@@ -229,6 +260,8 @@ extension QuizViewController {
             hideButtons()
         } else {
             showButtons()
+            // currentPageIndex가 0인 경우 previousButton을 숨깁니다.
+            previousButton?.isHidden = (currentPageIndex == 0)
         }
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
             self.view.layoutIfNeeded()
