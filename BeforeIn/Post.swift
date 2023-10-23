5d52514bb558d9c816fdcb89543614a08c0f9fb3
chanho lee <leech3878@naver.com>
2023-10-23 16:43:33 +0900

    chore: Post 구조체에 postID 추가

--- BeforeIn/Post.swift
+++ BeforeIn/Post.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 struct Post{
-let writer: String // email
-let writerNickName: String
-let title: String
-let content: String
-let comments: [Comment]
-let likes: Int
-let category: String
-let postingTime: Date
+    let writer: String // email
+    let writerNickName: String
+    let postID: String
+    let title: String
+    let content: String
+    let comments: [Comment]
+    let likes: Int
+    let category: String
+    let postingTime: Date
 }
