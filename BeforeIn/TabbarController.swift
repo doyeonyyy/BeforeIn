0b3b2cd55579deb84a44721c600eba87224be3b1
sanc93 <sanc93@me.com>
2023-10-26 20:47:11 +0900

    chore: 탭바 타이틀 표시

--- BeforeIn/TabbarController.swift
+++ BeforeIn/TabbarController.swift
@@ -46,10 +46,10 @@ class TapbarController: UITabBarController {
         let vc2 = UINavigationController(rootViewController: SearchViewController())
         let vc3 = UINavigationController(rootViewController: CommunityViewController())
         let vc4 = UINavigationController(rootViewController: ProfileViewController())
-        vc1.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
-        vc2.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "books.vertical"), selectedImage: UIImage(systemName: "books.vertical.fill"))
-        vc3.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "captions.bubble"), selectedImage: UIImage(systemName: "captions.bubble.fill"))
-        vc4.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+        vc1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        vc2.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "books.vertical"), selectedImage: UIImage(systemName: "books.vertical.fill"))
+        vc3.tabBarItem = UITabBarItem(title: "커뮤니티", image: UIImage(systemName: "captions.bubble"), selectedImage: UIImage(systemName: "captions.bubble.fill"))
+        vc4.tabBarItem = UITabBarItem(title: "내 정보", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
 
 
         setViewControllers([vc1, vc2, vc3, vc4], animated: false)
