5ad4082c0ee82afdb840f8afd0fd0b0e9b574d60
sanc93 <sanc93@me.com>
2023-10-26 11:47:54 +0900

    chore: Label 쪼개기, 글자 크기 및 간격 조정

--- BeforeIn/QuizPage/QuizIntroView.swift
+++ BeforeIn/QuizPage/QuizIntroView.swift
@@ -13,6 +13,7 @@ class QuizIntroView: UIView {
     private var labelB = UILabel()
     let labelC = UILabel()
     let labelD = UILabel()
+    let labelE = UILabel()
     let imageView = UIImageView()
     let skipButton = UIButton()
     let startButton = UIButton()
@@ -43,6 +44,7 @@ class QuizIntroView: UIView {
         addSubview(labelB)
         addSubview(labelC)
         addSubview(labelD)
+        addSubview(labelE)
         addSubview(imageView)
         addSubview(skipButton)
         addSubview(startButton)
@@ -53,30 +55,37 @@ class QuizIntroView: UIView {
         labelA.textColor = .black
         labelA.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 24)
         labelA.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide).offset(136)
+            make.top.equalTo(safeAreaLayoutGuide).offset(106)
             make.leading.equalTo(safeAreaLayoutGuide).offset(24)
         }
         
         labelB.textColor = .black
-        labelB.font = UIFont.boldSystemFont(ofSize: 38)
+        labelB.font = UIFont.boldSystemFont(ofSize: 28)
         labelB.snp.makeConstraints { make in
             make.leading.equalTo(labelA.snp.trailing).offset(8)
             make.bottom.equalTo(labelA.snp.bottom)
         }
-        labelC.text = "내                    을 확인해볼까요?"
+        labelC.text = "내"
         labelC.textColor = .black
         labelC.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 24)
         labelC.snp.makeConstraints { make in
             make.top.equalTo(labelA.snp.bottom).offset(8)
             make.leading.equalTo(safeAreaLayoutGuide).offset(24)
-            make.trailing.equalTo(safeAreaLayoutGuide).offset(50)
+//            make.trailing.equalTo(safeAreaLayoutGuide).offset(50)
         }
         labelD.text = "에티켓 레벨"
         labelD.textColor = .black
-        labelD.font = UIFont.boldSystemFont(ofSize: 27)
+        labelD.font = UIFont.boldSystemFont(ofSize: 28)
         labelD.snp.makeConstraints { make in
             make.bottom.equalTo(labelC.snp.bottom)
-            make.leading.equalTo(labelC.snp.leading).offset(24)
+            make.leading.equalTo(labelC.snp.trailing).offset(8)
+        }
+        labelE.text = "을 확인해볼까요?"
+        labelE.textColor = .black
+        labelE.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 24)
+        labelE.snp.makeConstraints { make in
+            make.bottom.equalTo(labelD.snp.bottom)
+            make.leading.equalTo(labelD.snp.trailing)
         }
         imageView.image = UIImage(named: "QuizIntroImage")
         imageView.snp.makeConstraints { make in
