9b627731d217525640d47828ef40d7d6dbe6f595
Leehan-sol <ho20128@naver.com>
2023-10-31 14:20:28 +0900

    chore: 프로필 페이지 테이블뷰 수정

--- BeforeIn/ProfilePage/AppInfoView/AppInfoViewController.swift
+++ BeforeIn/ProfilePage/AppInfoView/AppInfoViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 import SnapKit
 
-class AppInfoViewController: UIViewController {
+class AppInfoViewController: BaseViewController {
     
     // MARK: - Properties
-    let AppInfoCellList = ["공지사항","서비스 이용약관", "개인정보 처리방침", "피드백 남기기"]
+    let AppInfoCellList = ["앱 버전", "공지사항", "서비스 이용약관", "개인정보 처리방침", "피드백 남기기"]
+    
     let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isScrollEnabled = false
         return tableView
     }()
     
@@ -24,7 +26,7 @@ class AppInfoViewController: UIViewController {
         configureUI()
         setTableView()
     }
-
+    
     // MARK: - Methods
     private func configureUI() {
         
@@ -53,9 +55,13 @@ class AppInfoViewController: UIViewController {
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "AppInfoCell")
     }
+
+    
     
 }
 
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
 extension AppInfoViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -66,20 +72,27 @@ extension AppInfoViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AppInfoCell", for: indexPath)
         cell.textLabel?.text = AppInfoCellList[indexPath.row]
         cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
-        cell.accessoryType = .disclosureIndicator
-
+        
+        if indexPath.row == 0 {
+            cell.accessoryType = .none
+        } else {
+            cell.textLabel?.text = AppInfoCellList[indexPath.row]
+            cell.accessoryType = .disclosureIndicator
+        }
+        
         return cell
     }
     
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         if indexPath.row == 0 {
-           let TermsAndConditionsVC = TermsAndConditionsViewController()
-           self.navigationController?.pushViewController(TermsAndConditionsVC, animated: true)
-       } else if indexPath.row == 1 {
-           let PrivacyPolicyVC = PrivacyPolicyViewController()
-           self.navigationController?.pushViewController(PrivacyPolicyVC, animated: true)
-       }
+            let TermsAndConditionsVC = TermsAndConditionsViewController()
+            self.navigationController?.pushViewController(TermsAndConditionsVC, animated: true)
+        } else if indexPath.row == 1 {
+            let PrivacyPolicyVC = PrivacyPolicyViewController()
+            self.navigationController?.pushViewController(PrivacyPolicyVC, animated: true)
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
