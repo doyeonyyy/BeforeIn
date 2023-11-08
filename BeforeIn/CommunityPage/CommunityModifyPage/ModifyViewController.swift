39f27ad9f5e395a922a3e74b783e714239951889
Daisy Kim <doyeonyyy@gmail.com>
2023-11-08 20:02:44 +0900

    feat: 게시물 수정페이지에서도 카테고리 변경이 가능하게 수정, 데이터 베이스에도 반영

--- BeforeIn/CommunityPage/CommunityModifyPage/ModifyViewController.swift
+++ BeforeIn/CommunityPage/CommunityModifyPage/ModifyViewController.swift
@@ -38,7 +38,7 @@ class ModifyViewController: UIViewController {
     
     @objc func dailyButtonTapped() {
         modifyVeiw.dailyButton.isSelected = !modifyVeiw.dailyButton.isSelected // 반전 선택 상태
-
+        
         if modifyVeiw.dailyButton.isSelected {
             // 버튼이 선택된 경우
             modifyVeiw.dailyButton.setTitleColor(.white, for: .normal)
@@ -52,10 +52,10 @@ class ModifyViewController: UIViewController {
         }
         print("일상잡담 카테고리 선택")
     }
-
+    
     @objc func qnaButtonTapped() {
         modifyVeiw.qnaButton.isSelected = !modifyVeiw.qnaButton.isSelected
-
+        
         if modifyVeiw.qnaButton.isSelected {
             modifyVeiw.qnaButton.setTitleColor(.white, for: .normal)
             modifyVeiw.qnaButton.backgroundColor = UIColor.BeforeInRed
@@ -72,23 +72,40 @@ class ModifyViewController: UIViewController {
         guard let title = modifyVeiw.mainTextField.text else { return }
         guard let content = modifyVeiw.contentTextView.text else { return }
         guard let postID = post?.postID else { return }
-        db.collection("Post").document(postID).setData(["title": title, "content": content], merge: true)
+        
+        // 선택한 카테고리를 확인
+        var category = ""
+        if modifyVeiw.dailyButton.isSelected {
+            category = "일상잡담"
+        } else if modifyVeiw.qnaButton.isSelected {
+            category = "궁금해요"
+        }
+        
+        // 업데이트할 데이터를 설정
+        let updateData: [String: Any] = [
+            "title": title,
+            "content": content,
+            "category": category
+        ]
+        
+        // 데이터베이스에 업데이트
+        db.collection("Post").document(postID).setData(updateData, merge: true)
+        
         self.navigationController?.popViewController(animated: true)
     }
 }
-
 extension ModifyViewController: UITextViewDelegate{
     func textViewDidBeginEditing(_ textView: UITextView) {
-            if textView.textColor == UIColor.placeholderText {
-                textView.text = nil
-                textView.textColor = UIColor.black
-            }
-            
+        if textView.textColor == UIColor.placeholderText {
+            textView.text = nil
+            textView.textColor = UIColor.black
         }
-        func textViewDidEndEditing(_ textView: UITextView) {
-            if textView.text.isEmpty {
-                textView.text = "메세지를 입력하세요"
-                textView.textColor = UIColor.placeholderText
-            }
+        
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty && !textView.isFirstResponder {
+            textView.text = "메세지를 입력하세요"
+            textView.textColor = UIColor.placeholderText
         }
+    }
 }
