5e8fbc0488696123f057c56b29b5a5e30dbfd43f
sanc93 <sanc93@me.com>
2023-11-08 13:55:24 +0900

    chore: 코멘트 신고 이미 했다면 같은 아이디로 중복 신고 못하게

--- BeforeIn/CommunityPage/CommunityPage/CommunityPageViewController.swift
+++ BeforeIn/CommunityPage/CommunityPage/CommunityPageViewController.swift
@@ -349,27 +349,32 @@ extension CommunityPageViewController: UITableViewDataSource {
         let alert = UIAlertController(title: "이 댓글을 신고하시겠습니까?", message: "신고하시면 취소가 불가능합니다.", preferredStyle: .alert)
         let ok = UIAlertAction(title: "신고" , style: .destructive) { _ in
             for i in 0..<self.post.comments.count{
-               if comment == self.post.comments[i] {
-                   // TODO: - 이미 신고했다면 중복신고 불가(reportUserList에 이미 아이디가 들어가 있다면 이미 신고했다고 얼럿 띄우고 추가 안되게 돌리기..)
-                   self.post.comments[i].reportUserList.append(currentUser.email)
-                   let commentsData: [[String: Any]] = self.post.comments.map { comment in
-                       return [
-                           "writer": comment.writer,
-                           "writerNickName": comment.writerNickName,
-                           "content": comment.content,
-                           "reportUserList": comment.reportUserList,
-                           "postingTime": comment.postingTime
-                       ]
-                   }
-                   self.db.collection("Post").document(self.post.postID).updateData(["comments": commentsData]) { error in
-                       if let error = error {
-                           print("Error updating comments in Firestore: \(error.localizedDescription)")
-                       } else {
-                           print("추가 성공")
-                       }
-                   }
-                   break
-               }
+                if comment == self.post.comments[i] {
+                    if !self.post.comments[i].reportUserList.contains(currentUser.email){
+                        self.post.comments[i].reportUserList.append(currentUser.email)
+                        let commentsData: [[String: Any]] = self.post.comments.map { comment in
+                            return [
+                                "writer": comment.writer,
+                                "writerNickName": comment.writerNickName,
+                                "content": comment.content,
+                                "reportUserList": comment.reportUserList,
+                                "postingTime": comment.postingTime
+                            ]
+                        }
+                        self.db.collection("Post").document(self.post.postID).updateData(["comments": commentsData]) { error in
+                            if let error = error {
+                                print("Error updating comments in Firestore: \(error.localizedDescription)")
+                            } else {
+                                print("추가 성공")
+                            }
+                        }
+                        break
+                    } else {
+                        let alert = UIAlertController(title: "신고모ㅅ해요", message: "이미신고햇거든여", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "확인", style: .default))
+                        self.present(alert, animated: true)
+                    }
+                }
             }
         }
         let cancel = UIAlertAction(title: "취소", style: .cancel)
