6b1badacf9cb0c83abadfccc8e1b5555d8d439e3
Daisy Kim <doyeonyyy@gmail.com>
2023-11-08 15:31:15 +0900

    feat: 게시물 작성페이지 카테고리 옵션버튼 활성화

--- BeforeIn/CommunityPage/CommunityWritePage/WriteViewController.swift
+++ BeforeIn/CommunityPage/CommunityWritePage/WriteViewController.swift
@@ -15,8 +15,45 @@ class WriteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTarget()
+        
+    }
+    func addTarget() {
         writeView.contentTextView.delegate = self
         writeView.confirmButton.addTarget(self, action: #selector(confirmButtonClick), for: .touchUpInside)
+        writeView.dailyButton.addTarget(self, action: #selector(dailyButtonTapped), for: .touchUpInside)
+        writeView.qnaButton.addTarget(self, action: #selector(qnaButtonTapped), for: .touchUpInside)
+    }
+    @objc func dailyButtonTapped() {
+        writeView.dailyButton.isSelected = !writeView.dailyButton.isSelected // 반전 선택 상태
+
+        if writeView.dailyButton.isSelected {
+            // 버튼이 선택된 경우
+            writeView.dailyButton.setTitleColor(.white, for: .normal)
+            writeView.dailyButton.backgroundColor = UIColor.BeforeInRed
+            writeView.qnaButton.setTitleColor(UIColor.BeforeInRed, for: .normal)
+            writeView.qnaButton.backgroundColor = .white
+        } else {
+            // 버튼이 선택되지 않은 경우
+            writeView.dailyButton.setTitleColor(UIColor.BeforeInRed, for: .normal)
+            writeView.dailyButton.backgroundColor = .white
+        }
+        print("일상잡담 카테고리 선택")
+    }
+
+    @objc func qnaButtonTapped() {
+        writeView.qnaButton.isSelected = !writeView.qnaButton.isSelected
+
+        if writeView.qnaButton.isSelected {
+            writeView.qnaButton.setTitleColor(.white, for: .normal)
+            writeView.qnaButton.backgroundColor = UIColor.BeforeInRed
+            writeView.dailyButton.setTitleColor(UIColor.BeforeInRed, for: .normal)
+            writeView.dailyButton.backgroundColor = .white
+        } else {
+            writeView.qnaButton.setTitleColor(UIColor.BeforeInRed, for: .normal)
+            writeView.qnaButton.backgroundColor = .white
+        }
+        print("궁금해요 카테고리 선택")
     }
     
     @objc func confirmButtonClick() {
