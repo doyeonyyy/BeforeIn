7889a26dbdb96da59bd3e1b9ac1d69e93c12f4eb
sanc93 <sanc93@me.com>
2023-11-09 19:03:22 +0900

    chore: 커뮤니티페이지 글 리스트 UI 조정

--- BeforeIn/CommunityPage/CommunityMainPage/CustomCell/PostCell.swift
+++ BeforeIn/CommunityPage/CommunityMainPage/CustomCell/PostCell.swift
@@ -17,20 +17,29 @@ class PostCell: UITableViewCell {
     
     private let titleLable = UILabel().then{
         $0.text = "요즘 애들은 지하철 노약자석에 그냥 앉아 있나요?"
-        $0.font = UIFont.boldSystemFont(ofSize: 16)
+        $0.font = UIFont.systemFont(ofSize: 15)
         $0.numberOfLines = 0
+    }
+    
+    private let categoryButton = UIButton().then {
+        $0.setTitle("", for: .normal)
+        $0.setTitleColor(UIColor.darkGray, for: .normal)
+        $0.backgroundColor = UIColor.systemGray6
+        $0.layer.cornerRadius = 5
+        $0.contentEdgeInsets = UIEdgeInsets(top: 2, left: 6, bottom: 2, right: 6)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 10)
     }
     
     private let nickNameLabel = UILabel().then{
         $0.text = "닉네임 • "
         $0.textColor = .systemGray
-        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.font = UIFont.systemFont(ofSize: 12)
     }
     
     private let postingTimeLabel = UILabel().then{
         $0.text = "1일 전"
         $0.textColor = .systemGray
-        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.font = UIFont.systemFont(ofSize: 12)
     }
     
 //    private let heartImageView = UIImageView().then{
@@ -50,7 +59,7 @@ class PostCell: UITableViewCell {
     
     private let commentsLabel = UILabel().then{
         $0.text = "13"
-        $0.font = UIFont.systemFont(ofSize: 13)
+        $0.font = UIFont.systemFont(ofSize: 12)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -76,6 +85,7 @@ class PostCell: UITableViewCell {
     private func setupUI() {
         addSubview(postView)
         postView.addSubview(titleLable)
+        postView.addSubview(categoryButton)
         postView.addSubview(nickNameLabel)
         postView.addSubview(postingTimeLabel)
 //        postView.addSubview(heartImageView)
@@ -84,25 +94,30 @@ class PostCell: UITableViewCell {
         postView.addSubview(commentsLabel)
         
         postView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview().inset(8)
+            make.top.bottom.equalToSuperview().inset(6)
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         titleLable.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
-            make.top.equalToSuperview().inset(20)
+            make.top.equalToSuperview().inset(8)
 //            make.width.equalTo(329)
 //            make.height.equalTo(44)
         }
         
+        categoryButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLable)
+            make.trailing.equalToSuperview().offset(5)
+        }
+        
         nickNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLable.snp.bottom).offset(20)
+            make.top.equalTo(titleLable.snp.bottom).offset(10)
             make.leading.equalToSuperview()
         }
         
         postingTimeLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLable.snp.bottom).offset(20)
+            make.top.equalTo(titleLable.snp.bottom).offset(10)
             make.leading.equalTo(nickNameLabel.snp.trailing)
         }
         
@@ -118,15 +133,15 @@ class PostCell: UITableViewCell {
 //        }
         
         commentImageView.snp.makeConstraints { make in
-            make.top.equalTo(titleLable.snp.bottom).offset(20)
-            make.width.height.equalTo(16)
-            make.trailing.equalTo(commentsLabel.snp.leading).offset(-5)
+            make.top.equalTo(titleLable.snp.bottom).offset(10)
+            make.width.height.equalTo(14)
+            make.trailing.equalTo(commentsLabel.snp.leading).offset(-10)
         }
         
         commentsLabel.snp.makeConstraints { make in
             make.centerY.equalTo(commentImageView)
             make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-5)
+            make.bottom.equalToSuperview().offset(-10)
         }
     }
     
@@ -136,6 +151,7 @@ class PostCell: UITableViewCell {
         postingTimeLabel.text = post.postingTime.getTimeText()
        // heartsLabel.text = String(post.likes)
         commentsLabel.text = String(post.comments.count)
+        categoryButton.setTitle(String(post.category), for: .normal)
     }
 
 }
