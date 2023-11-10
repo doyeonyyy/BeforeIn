9981724bb8557c50f7bd57842ac82f666fe747ec
chanho lee <leech3878@naver.com>
2023-11-10 15:34:28 +0900

    chore: 수정 페이지 카테고리 버튼 로직 변경 및 post에서 cateogory 받아오는 로직 추가

--- BeforeIn/UIImage.swift
+++ BeforeIn/UIImage.swift
@@ -15,4 +15,15 @@ extension UIImage {
             self.draw(in: CGRect(origin: .zero, size: size))
         }
     }
+    
+    convenience init?(color: UIColor) {
+        let size = CGSize(width: 1, height: 1)
+        UIGraphicsBeginImageContext(size)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(origin: .zero, size: size))
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+        self.init(cgImage: image.cgImage!)
+    }
 }
