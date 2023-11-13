3caf2cdf80b2bd8f5384d08b553a635c02c2a554
sanc93 <sanc93@me.com>
2023-11-13 14:33:37 +0900

    chore: "서비스 이용약관" 페이지 인디케이터 적용

--- BeforeIn/ProfilePage/AppInfoView/TermsAndConditionsViewController.swift
+++ BeforeIn/ProfilePage/AppInfoView/TermsAndConditionsViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import WebKit
+import NVActivityIndicatorView
 
 class TermsAndConditionsViewController: BaseViewController, WKNavigationDelegate {
     // MARK: - Properties
@@ -15,10 +16,16 @@ class TermsAndConditionsViewController: BaseViewController, WKNavigationDelegate
         $0.backgroundColor = .systemBackground
     }
     
+    private let indicator = NVActivityIndicatorView(frame: CGRect(x: 162, y: 100, width: 50, height: 50),
+                                            type: .lineSpinFadeLoader,
+                                            color: .BeforeInRed,
+                                            padding: 0)
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(indicator)
         
         webView.navigationDelegate = self
    
@@ -33,11 +40,23 @@ class TermsAndConditionsViewController: BaseViewController, WKNavigationDelegate
     private func configureUI() {
         // addSubView
         view.addSubview(webView)
+        view.addSubview(indicator)
         
         // makeConstraints
         webView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.leading.trailing.bottom.equalToSuperview()
         }
+        indicator.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        indicator.startAnimating()
+    }
+        
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.stopAnimating()
     }
 }
